b2a84754a7dccd40b3e27dd4c086007ae69495c5
Missletty8 <missletty8@gmail.com>
2022-02-09 17:06:49 -0700

    First Commit and this is a merge from

---
+++ DogWalkingAppAuth/Migrations/202202092340006_202202080408488_AutomaticMigration.cs
@@ -0,0 +1,16 @@
+﻿namespace DogWalkingAppAuth.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _202202080408488_AutomaticMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
